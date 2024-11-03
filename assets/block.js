21805228f84abaa2c3cde17b57aadec2a90d982d
Seth <seth@smartwebutah.com>
2024-11-03 14:16:02 -0700

    Pass the game state and role id variables to the new game button

--- assets/block.js
+++ assets/block.js
@@ -6,31 +6,53 @@
         icon: 'games',
         category: 'widgets',
         attributes: {
-            content: {
-                type: 'array',
-                source: 'children',
-                selector: 'p',
+            role_id: {
+                type: 'string',
+                default: ''
+            },
+            game_state_id: {
+                type: 'string',
+                default: ''
             }
         },
         
         edit: function(props) {
-            return el(
-                'div',
-                { className: props.className },
-                el(components.Placeholder, {
-                    icon: 'games',
-                    label: 'Adventure Game',
-                },
-                el('p', {}, 'Your exciting adventure awaits! This block will display the Adventure Game when viewed on the front end.'),
-                el(components.Button, {
-                    isPrimary: true,
-                    onClick: function() {
-                        // This is where you could add functionality to configure the game
-                        alert('Game configuration would go here!');
-                    }
-                }, 'Configure Game')
+            var InspectorControls = editor.InspectorControls;
+            var TextControl = components.TextControl;
+
+            return [
+                el(
+                    InspectorControls,
+                    { key: 'inspector' },
+                    el(TextControl, {
+                        label: 'Role ID',
+                        value: props.attributes.role_id,
+                        onChange: function(newVal) {
+                            props.setAttributes({ role_id: newVal });
+                        }
+                    }),
+                    el(TextControl, {
+                        label: 'Game State ID',
+                        value: props.attributes.game_state_id,
+                        onChange: function(newVal) {
+                            props.setAttributes({ game_state_id: newVal });
+                        }
+                    })
+                ),
+                el(
+                    'div',
+                    { className: props.className },
+                    el(components.Placeholder, {
+                        icon: 'games',
+                        label: 'Adventure Game',
+                    },
+                    el('p', {}, 'Your exciting adventure awaits! This block will display the Adventure Game when viewed on the front end.'),
+                    el(components.Button, {
+                        isPrimary: true,
+                    }, 'Configure Game')
+                    )
                 )
-            );
+            ];
         },
         
         save: function() {
